2065a25eddaf0de254fb11c39ca49460f718d2a6
Philipp Bulyga <phillbleck@gmail.com>
2020-10-29 22:39:32 +0300

    add error monitor

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors')
-const products = require('./data/products');
-const connectDB = require('./config/DB')
+const connectDB = require('./config/DB');
+const productRoutes = require('./routes/product.routes')
+const {notFound, errorHandler} = require('./middleware/error.middleware');
 
 dotenv.config()
 
@@ -16,14 +17,10 @@ app.get('/', (req, res) => {
     res.send('API is running...')
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use('/api/products', productRoutes);
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(PORT, () => {
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.underline.bold)
