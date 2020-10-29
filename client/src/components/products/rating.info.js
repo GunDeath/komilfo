35996bce9cabb507623813c696063e2618f17e63
Philipp Bulyga <phillbleck@gmail.com>
2020-10-29 12:55:43 +0300

    Add start reting

---
+++ client/src/components/products/rating.info.js
@@ -0,0 +1,22 @@
+export const numArray = [
+    {
+        full: 1,
+        hulf: 0.5
+    },
+    {
+        full: 2,
+        hulf: 1.5
+    },
+    {
+        full: 3,
+        hulf: 2.5
+    },
+    {
+        full: 4,
+        hulf: 3.5
+    },
+    {
+        full: 5,
+        hulf: 4.5
+    }
+]
