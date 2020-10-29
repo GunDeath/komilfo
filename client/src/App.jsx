e6cec7c6d3032666b999b9a5047b3eebe2762412
Philipp Bulyga <phillbleck@gmail.com>
2020-10-29 11:58:33 +0300

    Add bootstrap and Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import {Container} from "react-bootstrap";
+import Header from "./components/Hedaer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+        <Header/>
+        <main className='py-3'>
+            <Container>
+                <h1>Komilfo Profession Shop</h1>
+            </Container>
+        </main>
+        <Footer/>
+    </>
   );
 }
 
