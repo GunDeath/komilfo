26ab667b6e06b7a64064ddd04e86cee38dcca336
Philipp Bulyga <phillbleck@gmail.com>
2020-10-29 16:30:03 +0300

    add dotenv module

--- server/server.js
+++ server/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv')
 const products = require('./data/products');
 
+dotenv.config()
+
 const app = express();
+
+const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
     res.send('API is running...')
@@ -16,6 +21,6 @@ app.get('/api/products/:id', (req, res) => {
     res.json(product)
 })
 
-app.listen(5000, () => {
-    console.log(`Server has been started at porn ${5000}`)
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 })
