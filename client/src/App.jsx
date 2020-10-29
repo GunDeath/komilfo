68d63adfa834b719c9854e86415a5976250ae6f9
Philipp Bulyga <phillbleck@gmail.com>
2020-10-29 12:21:41 +0300

    add products static

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {Container} from "react-bootstrap";
 import Header from "./components/Hedaer";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Header/>
         <main className='py-3'>
             <Container>
-                <h1>Komilfo Profession Shop</h1>
+                <HomeScreen/>
             </Container>
         </main>
         <Footer/>
