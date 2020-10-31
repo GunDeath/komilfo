de14e740ba5df7d53f30a145e287a6c40125056f
Philipp Bulyga <phillbleck@gmail.com>
2020-10-31 21:14:42 +0300

    build card

--- client/src/screens/CartScreen.jsx
+++ client/src/screens/CartScreen.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import {Row, Col, ListGroup, Image, Form, Button, Card} from "react-bootstrap";
+import {addToCart} from '../actions/cartAction'
 
-const CartScreen = () => {
+
+const CartScreen = ({match, location, history}) => {
+    const productID = match.params.id;
+
+    const qty = location.search ? Number(location.search.split('=')[1]) : 1;
+
+    const dispatch = useDispatch()
+
+    const cart = useSelector(state => state.cart)
+    const {cartItems} = cart
+    console.log(cartItems)
+
+    useEffect(() => {
+        if(productID){
+            dispatch(addToCart(productID, qty))
+        }
+    }, [dispatch, productID, qty])
+
     return(
-        <dev>
+        <div>
             Cart
-        </dev>
+        </div>
     )
 }
 
